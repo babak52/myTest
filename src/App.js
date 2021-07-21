2d3563b918f2b372c9084b9fb562e03570e6c253
babak <babak.sharifi52@gmail.com>
2021-07-21 21:27:27 +0200

    change app force console

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Form from "./component/form";
 import { BrowserRouter as Router, Switch, Route, Link ,  useParams,useRouteMatch} from "react-router-dom";
 
 function App() {
-  
+  console.log('force branch');
+
 
   return (
     <>
