361b7d13bf1c83784798aace65bcbf0a85eacbce
babak <babak.sharifi52@gmail.com>
2021-07-22 16:08:27 +0200

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Form from "./component/form";
 import { BrowserRouter as Router, Switch, Route, Link ,  useParams,useRouteMatch} from "react-router-dom";
 
 function App() {
-  console.log('force branch');
 
 
   return (
